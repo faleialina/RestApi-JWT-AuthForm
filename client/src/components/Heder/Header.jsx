a2650b37e9b4d823f8ad1ebb5460f2d55a649464
faleialina <august3@mail.ru>
2023-10-07 22:41:40 +0300

    headerLink

--- client/src/components/Heder/Header.jsx
+++ client/src/components/Heder/Header.jsx
@@ -1,10 +1,11 @@
+import { Link } from 'react-router-dom';
 import style from './heder.module.scss'
 
 function Header() {
     return (
         <div className={style.wrapper}>
-            <p>Sign in</p>
-            <p>Sign Up</p>
+            <p><Link to={'/'}>Sign in</Link></p>
+            <p><Link to={'/reg'}>Sign Up</Link></p>
         </div>
     )
 }
